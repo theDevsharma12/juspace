dc10564f8af27b411d93fcc712d1c7a4d51fb23b
theDevsharma12 <107252406+theDevsharma12@users.noreply.github.com>
2023-09-11 20:56:24 +0530

    Add files via upload

---
+++ auth.js
@@ -0,0 +1,25 @@
+let login=document.getElementById("login")
+login.addEventListener("click",async (e)=>{
+    e.preventDefault()
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    const response = await fetch(
+        "https://juspace.onrender.com/api/v1/auth/login",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email,
+            password,
+          }),
+        }
+      );
+  
+      const result = await response.json();
+      console.log(result)
+      localStorage.setItem("token",result.token)
+      
+      window.location.href="/"
+})
---
+++ script.js
@@ -0,0 +1,64 @@
+let token=localStorage.getItem("token")
+if (!token){
+    window.location.href="/login.html"
+}
+let firstname=localStorage.getItem("firstname")
+let username=document.getElementById("username")
+username.innerText=`Hi! ${firstname}`
+const navMenu = document.querySelector("nav");
+const hamburgerMenu = document.getElementById("hamburger-menu");
+const sideNav = document.querySelector(".side-nav");
+const main = document.querySelector("main");
+let activeBtn = document.querySelector(".study-material-section .sm-nav .active");
+let btnsStudyMaterial = document.querySelectorAll(".study-material-section .sm-nav li");
+let slider = document.querySelector(".study-material-section .slider");
+const container = document.querySelector(".study-material-section .container");
+let faqs= document.querySelectorAll(".faq");
+
+hamburgerMenu.onclick = () => {
+    navMenu.classList.toggle("active");
+};
+
+function disableScroll() {
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+
+    window.onscroll = function() {
+        window.scrollTo(scrollLeft, scrollTop);
+    };
+}
+
+function enableScroll() {
+    window.onscroll = function() {};
+}
+
+sideNav.onmouseenter = () => {
+    disableScroll();
+}
+
+sideNav.onmouseleave = () => {
+    enableScroll();
+}
+
+btnsStudyMaterial.forEach(btn => {
+    btn.addEventListener("click", event => {
+        activeBtn.classList.remove("active");
+        event.target.classList.add("active");
+        activeBtn = event.target;
+        let i = getComputedStyle(event.target).getPropertyValue('--i');
+        slider.style.transform = `translateX(calc(-${container.offsetWidth}px * ${i}))`;
+    });
+});
+
+
+faqs.forEach(elem => {
+    elem.addEventListener("click", () => {
+        elem.classList.toggle("active");
+    });
+});
+let logout=document.getElementById("logout")
+logout.addEventListener("click",async (e)=>{
+    e.preventDefault()
+    localStorage.removeItem("token")
+    window.location.href="/login.html"
+})
