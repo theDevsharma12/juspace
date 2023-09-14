b8b38772c7bf82591b9a5d8a6f119eb801d3e89f
devsharma12 <107252406+theDevsharma12@users.noreply.github.com>
2023-09-14 11:57:31 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -21,7 +21,7 @@ login.addEventListener("click",async (e)=>{
       console.log(result)
       localStorage.setItem("token",result.token)
       
-      window.location.href="/login.html"
+      window.location.href="login.html"
     //juspace/juspace.tech
 })
 
